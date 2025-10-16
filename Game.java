94ced9b5146a89192b9ca2ab9d329d2401628e4a
FilipAnger <filip.anger@gmail.com>
2025-10-16 12:21:39 +0200

    Revert "Auto stash before merge of "Item/PickUp" and "origin/Item/PickUP2""
    This reverts commit c7f967130152a71487b6b5a767bb5a9f76565ea4.

--- Game.java
+++ Game.java
@@ -20,28 +20,18 @@ class Game {
 
     GamePanel gamePanel;
     JFrame frame;
-    int fps;
-    
-    @Override
-    public void run() {
-        JFrame f = new JFrame("Savior of the Logic");
-        f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        f.setSize(WIDTH, HEIGHT);
-        f.setLayout(new BorderLayout());
-        GamePanel gp = new GamePanel(0, 0, this.fps);
-        f.add(gp, BorderLayout.CENTER);
 
-        this.frame = f;
-        this.gamePanel = gp;
-        f.setVisible(true);
-        
-        };
+    void start() {
+        this.frame = new JFrame("Savior of the Logic");
+        this.frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.frame.setSize(WIDTH, HEIGHT);
+        this.frame.setLayout(new BorderLayout());
 
-    public Game(int fps) {
-        this.fps = fps;
-        SwingUtilities.invokeLater(this);
-    }  
+        this.gamePanel = new GamePanel(20  , 100);
+        this.frame.add(this.gamePanel, BorderLayout.CENTER);
 
+        this.frame.setVisible(true);
+    }
     
     public static void main(String[] args) {
         Game game = new Game();
